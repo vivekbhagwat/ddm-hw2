304acdcaeba6e2b5ea28193c983cacc1fc709223
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2012-03-19 20:27:12 -0400

    loads text file, splits randomly into train/test

--- polynomial_regression/regression.py
+++ polynomial_regression/regression.py
@@ -1,2 +1,16 @@
-import numpy
+import numpy as np
+from numpy.random import permutation
+
+orig_mat = np.loadtxt('polyfit.tsv',skiprows=1)
+
+orig_x = orig_mat[:,0]
+orig_y = orig_mat[:,1]
+
+new_mat = permutation(orig_mat)
+
+train = new_mat[0:100,:]
+test = new_mat[100:200,:]
+
+
+
 
